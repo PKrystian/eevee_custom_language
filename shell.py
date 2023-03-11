05f0b481ee48bff41279dd21f1264229311e4ce1
PKrystian <krystian.pinczak2001@gmail.com>
2023-03-11 20:26:12 +0100

    Added basic lexer and updated shell input. User input from .eevee file

--- shell.py
+++ shell.py
@@ -1,3 +1,5 @@
-while True:
-    user_input = input("eevee :> ")
-    print(user_input)
+from lexer import lexer
+
+User_Commands = open("user_text.eevee", "r").readlines()
+for line in User_Commands:
+    lexer(line)
